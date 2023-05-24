12d6450ec38278a962910d45f9c1232614eb243e
Chinika Charles <clcharles@hotmail.co.uk>
2023-05-25 00:36:12 +0100

    Added in react router

--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -1,2 +1,20 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+        children: [
+            {
+                path: "/MyProjects",
+                element: <MyProjects />,
+            },
+            {
+                path: "/Contact",
+                element: <Contact />,
+            }
+        ]
+    }
+])
 export default PortfolioContainer;
