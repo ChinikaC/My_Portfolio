ad063f9f7fe7da7555faea25201a0c878deb3ff9
Chinika Charles <clcharles@hotmail.co.uk>
2023-05-31 01:45:44 +0100

    Added in an about section

---
+++ src/components/AboutMe.js
@@ -0,0 +1,45 @@
+const AboutMe = () => {
+    return (
+        <div className="about">
+            <div>
+                <h1> Chinika Charles</h1>
+            </div>
+            <div>
+                <p>I am a Graduate/Junior Software Engineer looking for her first role in Software Engineering.</p>
+            </div>
+            <div>
+                <h2>About Me:</h2>
+                <p> I live in Buckinghamshire, however, I am more than happy to commute for work. </p>
+                <p>I have worked in several customer facing roles in healthcare, entertainment and hospitality.</p>
+                <p>I have done volunteering and mentoring in the past.</p>
+                
+                <h3> Timeline </h3>
+                <p> 2018-2021: I studied Psychology at Queen Mary University of London and graduated with a First Class Honours.</p>
+                
+                <p>Nov 2021: Started working as a Medical Receptionist at an Orthopaedics and Sports Medicine Clinic.
+                    I originally wanted to become a Sport Psychologist so before I underwent my MSc I wanted to work, save
+                    and gain work experience.</p>
+
+                <p>Dec 2021: Began volunteering at a local athletics club, assisting the coaches and working with athletes
+                        aged 9 to 14 years old.</p>
+
+                <p>May 2022: Started broadening my career options and formed an interest and curiosity for technology, in particular,
+                        web development and signed up for my first "Intro to Coding" workshop. This is where my true passion for software engineering 
+                        began.</p>
+
+                <p>May - Oct 2022: Signed up for several coding and web development workshops, events and courses.</p>
+
+                <p>Sept - Nov 2022: Began an eight-week "Introduction to Web Development" course with Code First Girls.</p>
+
+                <p>Jan 2023: Got accepted onto the 12-week Bright Network Technology Academy Full Stack Software Engineering Bootcamp.
+                        I was taught several programming languages, frameworks and libraries including Java, SQL, PostgreSQL, Spring Boot,
+                        HTML, CSS, JavaScript, React, as well as, Jest, JUnit, unit testing, object-oriented programming (OOP), test-driven development (TDD),
+                        and Git.</p>
+
+                <p>April 2023: Successfully completed and graduated from the bootcamp. Now open to work opportunities.</p>
+            </div>
+        </div>
+    )
+};
+
+export default AboutMe;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom"
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import AboutMe from "../components/AboutMe";
 
 const Home = () => {
 
@@ -9,6 +10,9 @@ const Home = () => {
         <header>
             <Header/>
         </header>
+        <AboutMe>
+            <AboutMe/>
+        </AboutMe>
         <main>
             <Outlet />
         </main>
