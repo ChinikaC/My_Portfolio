6f1b9cfab8ebd3210f6bedfdcf0d7ca70d2aa659
Chinika Charles <clcharles@hotmail.co.uk>
2023-06-16 01:08:36 +0100

    Added Git, GitHub, RESTful API, IntelliJ and VSC icons

--- src/components/TechnologyStack.js
+++ src/components/TechnologyStack.js
@@ -8,13 +8,18 @@ import { FaReact } from "react-icons/fa";
 import { SiJavascript } from "react-icons/si";
 import { SiJunit5 } from "react-icons/si";
 import { SiJest } from "react-icons/si";
+import { BsGit } from "react-icons/bs";
+import { AiFillGithub } from "react-icons/ai";
+import { TbApi } from "react-icons/tb";
+import { SiIntellijidea } from "react-icons/si";
+import { SiVisualstudiocode } from "react-icons/si";
 
 const TechnologyStack = () => {
   return ( 
   
     <div className="technologyStackLogos"> 
     <h1>My Technology Stack</h1>
-    <h2>The tools, libraries and frameworks that I am familiar with:</h2>
+    <h2>The tools, libraries, frameworks and technologies that I am familiar with or worked with before:</h2>
     <ul>
     <li>
     Java <FaJava/>
@@ -41,11 +46,27 @@ const TechnologyStack = () => {
     React <FaReact/>
     </li>
     <li>
-      JUnit <SiJunit5/>
+    JUnit <SiJunit5/>
     </li>
     <li>
-      Jest <SiJest/>
+    Jest <SiJest/>
     </li>
+    <li>
+    Git <BsGit/>
+    </li>
+    <li>
+    GitHub <AiFillGithub/>
+    </li>
+    <li>
+    RESTful API <TbApi/>
+    </li>
+    <li>
+    IntelliJ <SiIntellijidea/>
+    </li>
+    <li>
+    Visual Studio Code <SiVisualstudiocode/>
+    </li>
+   
   
     </ul>
    </div>
