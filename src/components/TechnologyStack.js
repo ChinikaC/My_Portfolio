4b033e89cf52cb291da4125acea6c476e0b9253a
Chinika Charles <clcharles@hotmail.co.uk>
2023-06-17 00:41:43 +0100

    Added node.js to technology stack

--- src/components/TechnologyStack.js
+++ src/components/TechnologyStack.js
@@ -18,81 +18,85 @@ import { SiPostman } from "react-icons/si";
 import { GiElephant } from "react-icons/gi";
 import { BsTerminalFill } from "react-icons/bs";
 import { BsKanban } from "react-icons/bs";
+import { FaNodeJs } from "react-icons/fa";
 
 const TechnologyStack = () => {
-  return ( 
-  
-    <div className="technologyStackLogos"> 
-    <h1>My Technology Stack</h1>
-    <h2>The tools, libraries, frameworks and technologies that I am familiar with include:</h2>
-    <ul>
-    <li>
-    Java <FaJava/>
-    </li>
-    <li>
-    SQL <BsFiletypeSql/>
-    </li>
-    <li>
-    PostgreSQL <SiPostgresql/>
-    </li>
-    <li>
-    HTML <ImHtmlFive2 />
-    </li>
-    <li>
-    CSS <SiCss3/>
-    </li>
-    <li>
-    JavaScript <SiJavascript/>
-    </li>
-    <li>
-    Spring Boot <SiSpring/>
-    </li>
-    <li>
-    React <FaReact/>
-    </li>
-    <li>
-    JUnit <SiJunit5/>
-    </li>
-    <li>
-    Git <BsGit/>
-    </li>
-    <li>
-    GitHub <AiFillGithub/>
-    </li>
-    <li>
-    Postman <SiPostman/>
-    </li>
-    <li>
-    Postico <GiElephant/>
-    </li>
-    <li>
-    RESTful API <LuCloudCog/>
-    </li>
-    <li>
-    Terminal <BsTerminalFill/>
-    </li>
-    <li>
-    IntelliJ <SiIntellijidea/>
-    </li>
-    <li>
-    Visual Studio Code <SiVisualstudiocode/>
-    </li>
-    <li>
-    Kanban Board <BsKanban/>
-    </li>
-    </ul>
+  return (
 
-    <h3> And a little of: </h3>
-    <ul>
-    <li>
-    Python <FaPython/>
-    </li>
-    <li>
-    Jest <SiJest/>
-    </li>
-    </ul>
+    <div className="technologyStackLogos">
+      <h1>My Technology Stack</h1>
+      <h2>The tools, libraries, frameworks and technologies that I am familiar with include:</h2>
+      <ul>
+        <li>
+          Java <FaJava />
+        </li>
+        <li>
+          SQL <BsFiletypeSql />
+        </li>
+        <li>
+          PostgreSQL <SiPostgresql />
+        </li>
+        <li>
+          HTML <ImHtmlFive2 />
+        </li>
+        <li>
+          CSS <SiCss3 />
+        </li>
+        <li>
+          JavaScript <SiJavascript />
+        </li>
+        <li>
+          Spring Boot <SiSpring />
+        </li>
+        <li>
+          React <FaReact />
+        </li>
+        <li>
+          Node.js <FaNodeJs />
+        </li>
+        <li>
+          JUnit <SiJunit5 />
+        </li>
+        <li>
+          Git <BsGit />
+        </li>
+        <li>
+          GitHub <AiFillGithub />
+        </li>
+        <li>
+          Postman <SiPostman />
+        </li>
+        <li>
+          Postico <GiElephant />
+        </li>
+        <li>
+          RESTful API <LuCloudCog />
+        </li>
+        <li>
+          Terminal <BsTerminalFill />
+        </li>
+        <li>
+          IntelliJ <SiIntellijidea />
+        </li>
+        <li>
+          Visual Studio Code <SiVisualstudiocode />
+        </li>
+        <li>
+          Kanban <BsKanban />
+        </li>
+      </ul>
 
-   </div>
+      <h3> And a little of: </h3>
+      <ul>
+        <li>
+          Python <FaPython />
+        </li>
+        <li>
+          Jest <SiJest />
+        </li>
+      </ul>
+
+    </div>
   )
 };
 
