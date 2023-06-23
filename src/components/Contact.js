3c60ccbef3d91c15f266e2e976efb5217fa96f1e
Chinika Charles <clcharles@hotmail.co.uk>
2023-06-23 02:08:32 +0100

    Edited 'Contact' section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,22 @@
+import { BsLinkedin } from 'react-icons/bs';
+
 const Contact = () => {
+
+    return (
+        <div className="contactDetails">
+            <div>
+                <h1> Feel Free to Contact me On:</h1>
+            </div>
+            <div className="contactIcons">
+            <ul>
+        <li>
+          LinkedIn <BsLinkedin/>
+        </li>
+        </ul>
+
+            </div>
+        </div>
+    )
 
 };
 
